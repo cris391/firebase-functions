36b7237d95daef0db2a770554930bc56f318e691
cristian <cristianpand@yahoo.com>
2020-06-25 16:20:23 +0200

    filter added

--- functions/src/Models/Transaction.ts
+++ functions/src/Models/Transaction.ts
@@ -1,15 +1,15 @@
 export class Transaction {
 
-  constructor(userId: string, name: string, description: string, createdDate: number){
+  constructor(userId: string, name: string, description: string, status: string,createdDate: number){
     this.userId = userId;
     this.name = name;
     this.description = description;
-    // this.status = status;
+    this.status = status;
     this.createdDate = createdDate;
   }
   userId: string = '';
   name: string = '';
   description: string = ''
-  // status: string = '';
+  status: string = '';
   createdDate: number = 0;
 }
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,11 +1,8 @@
 import * as functions from 'firebase-functions';
+import { Transaction } from './Models/Transaction';
+import { TransactionStatus } from './Enums/TransactionStatus';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+
 
 export const getTransactions = functions.https.onRequest((request, response) => {
   if (request.method != 'POST') {
@@ -13,9 +10,96 @@ export const getTransactions = functions.https.onRequest((request, response) => 
     return;
   }
   const body = request.body;
-  if (!body.hasOwnProperty('userId') || !body.hasOwnProperty('fromDate') || !body.hasOwnProperty('toDate') ) {
+  if (!body.hasOwnProperty('userId') || !body.hasOwnProperty('fromDate') || !body.hasOwnProperty('toDate')) {
     response.send('false');
   }
-  // response.send(request.body.username);
-  response.send("true");
+  const transactionsFound = filterItems(transactions, {
+    userId: body.userId,
+    fromDate: body.fromDate,
+    toDate: body.toDate,
+  });
+
+  response.send(transactionsFound);
 });
+
+/////////
+// Helpers
+/////////
+function filterItems(arr: any, query: any) {
+  return arr.filter(function (el: any) {
+    return el.userId === query.userId && el.createdDate >= query.fromDate && el.createdDate <= query.toDate;
+  });
+}
+
+const transactions = [
+  new Transaction(
+    'user_ID_1',
+    'My First Transaction Name',
+    'My First Transaction Description',
+    TransactionStatus.success,
+    new Date('2020-06-19T03:24:00').getTime()
+  ),
+  new Transaction(
+    'user_ID_2',
+    'My Second Transaction Name',
+    'My Second Transaction Description',
+    TransactionStatus.pending,
+    new Date('2020-06-20T03:24:00').getTime()
+  ),
+  new Transaction(
+    'user_ID_1',
+    'My Third Transaction Name',
+    'My third Transaction Description',
+    TransactionStatus.failed,
+    new Date('2020-06-21T03:24:00').getTime()
+  ),
+  new Transaction(
+    'user_ID_1',
+    'My Fourth Transaction Name',
+    'My Fourth Transaction Description',
+    TransactionStatus.success,
+    new Date('2020-06-22T03:24:00').getTime()
+  ),
+  new Transaction(
+    'user_ID_1',
+    'My Fifth Transaction Name',
+    'My Fifth Transaction Description',
+    TransactionStatus.pending,
+    new Date('2020-06-23T03:24:00').getTime()
+  ),
+  new Transaction(
+    'user_ID_1',
+    'My Sixth Transaction Name',
+    'My Sixth Transaction Description',
+    TransactionStatus.failed,
+    new Date('2020-06-24T03:24:00').getTime()
+  ),
+  new Transaction(
+    'user_ID_1',
+    'My Seventh Transaction Name',
+    'My Seventh Transaction Description',
+    TransactionStatus.success,
+    new Date('2020-06-25T03:24:00').getTime()
+  ),
+  new Transaction(
+    'user_ID_1',
+    'My Eight Transaction Name',
+    'My Eight Transaction Description',
+    TransactionStatus.pending,
+    new Date('2020-06-26T03:24:00').getTime()
+  ),
+  new Transaction(
+    'user_ID_1',
+    'My Ninth Transaction Name',
+    'My Ninth Transaction Description',
+    TransactionStatus.failed,
+    new Date('2020-06-27T03:24:00').getTime()
+  ),
+  new Transaction(
+    'user_ID_1',
+    'My Tenth Transaction Name',
+    'My Tenth Transaction Description',
+    TransactionStatus.success,
+    new Date('2020-06-28T03:24:00').getTime()
+  ),
+];
